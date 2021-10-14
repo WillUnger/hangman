2613eb66d454d370d83f9fac2a979b05842278f5
WillUnger <williamkjunger@gmail.com>
2021-10-14 17:08:13 +0000

    letter list that makes up word with '-' and reveals word as player guesses correctly

--- run.py
+++ run.py
@@ -27,5 +27,27 @@ def game():
     word_letters = letter(word) #letters in the word
     used_letters = letter() #letters the user has guessed
     letters = ('abcdefghijklmnopqrstuvwxyz')
-    tries = 5 #number of tries the user has to guess word
     
+    letter_list = [letter if letter in used_letters
+    else '-' for letter in word]
+    print('Word: ', ''.join(letter_list))
+    
+    tries = 5 #number of tries the user has to guess word
+
+
+
+    input_letter = input('Choose a letter: ').upper() #player chooses a letter
+    if input_letter in letters - used_letters: 
+        used_letters.add(input_letter)
+        if input_letter in word_letters:
+            word_letters.remove(input_letter)
+    
+    elif input_letter in used_letters:
+        print("You have already used that letter")
+        
+    else:
+        print("You must choose a letter")
+
+
+
+ 
