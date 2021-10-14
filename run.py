7e322949f9c2f6a4924cbf23dc93dfa964c1a6d3
WillUnger <williamkjunger@gmail.com>
2021-10-14 16:34:24 +0000

    add game function that defines words and letters and, number of tries and the removal of letters that the player guesses

--- run.py
+++ run.py
@@ -10,21 +10,22 @@ player_name = input("Please enter your name:")
 print("Welcome + player_name, read the rules below then hit the start button to begin")
 
 
-# Hangman area is generated - This is where an illustration of the hangman is built with every
-# incorrect letter that is guessed
 
-# Computer generates random (hidden) word from a list, all below the hangman area
 
-# Player guesses the letter from a keyboard provided
+# Computer generates random word from a list
 
-# If letter is contained in the word then letter fills the blank of
-# where it appears within the word
+def get_random_word(random):
+    """This function generates the word that needs to be guessed """
 
-# If letter is not contained in the word then that letter is blanked out on the keyboard and player loses 
-# a try (total 5 tries)
+    word = random.choice(words)
 
-# If full word is guessed, message appears, f"Congratulations {player_name}! You guessed the correct word"
-# Play again? - Button below - Restarts game
+return word
 
-# If numbers of tries is < 1 then message appears, f"Sorry {player_name}...You died!"
-# Play again? - Button below - Restarts game
+
+def game():
+    word = get_random_word(words) 
+    word_letters = letter(word) #letters in the word
+    used_letters = letter() #letters the user has guessed
+    letters = ('abcdefghijklmnopqrstuvwxyz')
+    tries = 5 #number of tries the user has to guess word
+    
