40afe10768e814c6dc1e981d5ff0ce428ed709e9
WillUnger <williamkjunger@gmail.com>
2021-10-16 17:47:12 +0000

    add to the function and while loop the right guess and wrong guess with print statement to accompany

--- run.py
+++ run.py
@@ -33,12 +33,24 @@ def gallows_pole_area(tries):
     
 
 def guess_input():
-
+    """
+    This function is for the player to input a letter in order to try and guess the word
+    """
     while True:
         player_input = input("Guess A Letter: ")
         if player_input in alphabet:
             if player_input in guess_letters:
                 print("You Have Already Guessed That Letter, Try Again")
-           
+            else:
+                guess_letters.append(player_input)
+                if player_input in secret_word:
+                    print("Excellent! You Guessed A Correct Letter")
+                    for i in range(0, len(secret_word)):
+                        if secret_word[i] == player_input:
+                            word_space[i] = player_input
+                else:
+                    print("Wrong! That Letter Is Not In This Word")
+                    tries = tries + 1
 
+        
 
